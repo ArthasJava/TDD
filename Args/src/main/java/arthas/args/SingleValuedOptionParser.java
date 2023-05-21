43826841d38ba7bf38101ddf822eb2b7b165fae4
Arthas <arthasjava@foxmail.com>
2023-05-21 22:07:27 +0800

    feat(Args): 测试用例(Sad Path)：-p -l 和 -p 两种情况的测试

--- Args/src/main/java/arthas/args/SingleValuedOptionParser.java
+++ Args/src/main/java/arthas/args/SingleValuedOptionParser.java
@@ -13,6 +13,9 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        if (index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-")) {
+            throw new InsufficientArgumentException("p");
+        }
         if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-")) {
             throw new TooManyArgumentException("p");
         }
--- Args/src/test/java/arthas/args/SingleValuedOptionParserTest.java
+++ Args/src/test/java/arthas/args/SingleValuedOptionParserTest.java
@@ -1,8 +1,11 @@
 package arthas.args;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static arthas.args.BooleanParserTest.option;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +16,15 @@ class SingleValuedOptionParserTest {
     void should_not_accept_extra_argument_for_single_valued_option() {
         TooManyArgumentException exp = assertThrows(TooManyArgumentException.class,
                 () -> new SingleValuedOptionParser<>(Integer::parseInt).parse(Arrays.asList("-p", "8080", "8081"),
+                        option("p")));
+        assertEquals("p", exp.getOption());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-p -l", "-p"})
+    void should_not_accept_insufficient_argument_for_singe_valued_option(String arguments) {
+        InsufficientArgumentException exp = assertThrows(InsufficientArgumentException.class,
+                () -> new SingleValuedOptionParser<>(Integer::parseInt).parse(Arrays.asList(arguments.split(" ")),
                         option("p")));
         assertEquals("p", exp.getOption());
     }
