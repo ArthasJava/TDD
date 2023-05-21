734a49914ed0cca6b9bad21ab807a8147a588c05
Arthas <arthasjava@foxmail.com>
2023-05-21 18:10:08 +0800

    refactor(Args): 删除 StringOptionParser
    技巧：
    1. 通过 Inline 工厂方法，替换原来构造方法的调用处;
    2. 经过 Inline，StringOptionParser 没有被调用，可删除

--- Args/src/main/java/arthas/args/StringOptionParser.java
+++
@@ -1,8 +0,0 @@
-package arthas.args;
-
-class StringOptionParser extends IntOptionParser {
-
-    public static OptionParser createStringOptionParser() {
-        return new IntOptionParser(String::valueOf);
-    }
-}
