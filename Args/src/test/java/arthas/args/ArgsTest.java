102b2efe8a4113142e27bdf2473eac4790653033
Arthas <arthasjava@foxmail.com>
2023-05-21 16:43:50 +0800

    feat(Args): 测试用例：-l -p 8080 -d /usr/logs 解析成功

--- Args/src/test/java/arthas/args/ArgsTest.java
+++ Args/src/test/java/arthas/args/ArgsTest.java
@@ -3,34 +3,46 @@ package arthas.args;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArgsTest {
     @Test
     void should_set_boolean_option_to_true_when_flag_present() {
         BooleanOption option = Args.parse(BooleanOption.class, "-l");
-        Assertions.assertTrue(option.logging());
+        assertTrue(option.logging());
     }
 
     @Test
     void should_set_boolean_option_to_false_when_flag_not_present() {
         BooleanOption option = Args.parse(BooleanOption.class);
-        Assertions.assertFalse(option.logging);
+        assertFalse(option.logging);
     }
 
-    static record BooleanOption(@Option("l") boolean logging) { }
+    record BooleanOption(@Option("l") boolean logging) { }
 
     @Test
     void should_parse_int_as_option_value() {
         IntOption option = Args.parse(IntOption.class, "-p", "8080");
-        Assertions.assertEquals(8080, option.port);
+        assertEquals(8080, option.port);
     }
 
-    static record IntOption(@Option("p") int port) { }
+    record IntOption(@Option("p") int port) { }
 
     @Test
     void should_get_string_as_option_value() {
         StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
-        Assertions.assertEquals("/usr/logs", option.directory);
+        assertEquals("/usr/logs", option.directory);
     }
-    
-    static record StringOption(@Option("d") String directory) { }
+
+    record StringOption(@Option("d") String directory) { }
+
+    @Test
+    void should_parse_multi_options() {
+        MultiOption option = Args.parse(MultiOption.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(option.logging);
+        assertEquals(8080, option.port);
+        assertEquals("/usr/logs", option.directory);
+    }
+
+    record MultiOption(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) { }
 }
