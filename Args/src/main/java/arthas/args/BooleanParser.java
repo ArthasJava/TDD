8ce6038fedce314716ae0d0b142590116bb18da7
Arthas <arthasjava@foxmail.com>
2023-05-23 08:23:16 +0800

    refactor(Args): BooleanParser 使用静态工厂方法 + lambda 消除 new 方式新建对象

--- Args/src/main/java/arthas/args/BooleanParser.java
+++ Args/src/main/java/arthas/args/BooleanParser.java
@@ -1,13 +1,17 @@
 package arthas.args;
 
-import arthas.args.exception.TooManyArgumentException;
-
 import java.util.List;
 
 class BooleanParser implements OptionParser<Boolean> {
+    private BooleanParser() {
+    }
 
     @Override
     public Boolean parse(List<String> arguments, Option option) {
         return SingleValuedOptionParser.values(arguments, option, 0).map(it -> true).orElse(false);
+    }
+
+    public static OptionParser<Boolean> createBooleanParser() {
+        return ((arguments, option) -> SingleValuedOptionParser.values(arguments, option, 0).isPresent());
     }
 }
