a9f3c59d3e4cb8e4c82ba19df757e5e86b0b7676
Arthas <arthasjava@foxmail.com>
2023-05-23 08:04:41 +0800

    refactor(Args): SingleValuedOptionParser 抽取方法
    技巧：使用 Optional 进行重构！

--- Args/src/main/java/arthas/args/SingleValuedOptionParser.java
+++ Args/src/main/java/arthas/args/SingleValuedOptionParser.java
@@ -4,6 +4,7 @@ import arthas.args.exception.InsufficientArgumentException;
 import arthas.args.exception.TooManyArgumentException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
@@ -18,18 +19,29 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
 
     @Override
     public T parse(List<String> arguments, Option option) {
+        return values(arguments, option, 1).map(it -> parseValue(it.get(0))).orElse(defaultValue);
+    }
+
+    private static Optional<List<String>> values(List<String> arguments, Option option, int expectedSize) {
+        Optional<List<String>> argumentList;
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) {
-            return defaultValue;
+            argumentList = Optional.empty();
+        } else {
+            List<String> values = values(arguments, index);
+            if (values.size() < expectedSize) {
+                throw new InsufficientArgumentException(option.value());
+            }
+            if (values.size() > expectedSize) {
+                throw new TooManyArgumentException(option.value());
+            }
+            argumentList = Optional.of(values);
         }
-        List<String> values = values(arguments, index);
-        if (values.size() < 1) {
-            throw new InsufficientArgumentException(option.value());
-        }
-        if (values.size() > 1) {
-            throw new TooManyArgumentException(option.value());
-        }
-        return valueParser.apply(arguments.get(index + 1));
+        return argumentList;
+    }
+
+    private T parseValue(String value) {
+        return valueParser.apply(value);
     }
 
     static List<String> values(List<String> arguments, int index) {
