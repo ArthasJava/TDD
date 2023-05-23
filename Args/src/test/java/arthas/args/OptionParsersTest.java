49dc54efb83363a23d15bc865d7bcc55a65dbe4e
Arthas <arthasjava@foxmail.com>
2023-05-23 08:44:34 +0800

    refactor(Args): OptionParsersTest 新增 BoolOptionParser 内部类

--- Args/src/test/java/arthas/args/OptionParsersTest.java
+++ Args/src/test/java/arthas/args/OptionParsersTest.java
@@ -2,15 +2,18 @@ package arthas.args;
 
 import arthas.args.exception.InsufficientArgumentException;
 import arthas.args.exception.TooManyArgumentException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 
-import static arthas.args.BooleanParserTest.option;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OptionParsersTest {
@@ -48,5 +51,40 @@ class OptionParsersTest {
             Object whatever = new Object();
             assertSame(parsed, OptionParsers.unary(whatever, parse).parse(Arrays.asList("-p", "8080"), option("p")));
         }
+    }
+
+    @Nested
+    class BoolOptionParser {
+        @Test
+        void should_not_accept_extra_argument_for_boolean_option() {
+            TooManyArgumentException exp = assertThrows(TooManyArgumentException.class, () -> {
+                OptionParsers.bool().parse(Arrays.asList("-l", "t"), option("l"));
+            });
+            Assertions.assertEquals("l", exp.getOption());
+        }
+
+        @Test
+        void should_set_default_value_to_false_when_option_not_present() {
+            assertFalse(OptionParsers.bool().parse(Collections.EMPTY_LIST, option("l")));
+        }
+
+        @Test
+        void should_set_value_to_true_when_option_present() {
+            assertTrue(OptionParsers.bool().parse(List.of("-l"), option("l")));
+        }
+    }
+    static Option option(String value) {
+        return new Option() {
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return Option.class;
+            }
+
+            @Override
+            public String value() {
+                return value;
+            }
+        };
     }
 }
