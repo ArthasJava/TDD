371e74eb87c5af1c8aceff5dc15a5a38efe04dc9
Arthas <arthasjava@foxmail.com>
2023-12-29 08:56:41 +0800

    refactor(ContextConfig): 搬迁 getInjectConstructor 方法到 ConstructorInjectionSupplier，使得类的方法更加内聚

--- Di/src/main/java/arthas/tdd/di/ContextConfig.java
+++ Di/src/main/java/arthas/tdd/di/ContextConfig.java
@@ -33,23 +33,7 @@ public class ContextConfig {
     }
 
     public <Type, Implementation extends Type> void bind(Class<Type> type, Class<Implementation> implementation) {
-        Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
-        providers.put(type, new ConstructorInjectionSupplier<>(injectConstructor));
-    }
-
-    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
-        List<Constructor<?>> injectConstructors = stream(implementation.getConstructors()).filter(
-                c -> c.isAnnotationPresent(Inject.class)).collect(Collectors.toList());
-        if (injectConstructors.size() > 1) {
-            throw new IllegalComponentException();
-        }
-        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> {
-            try {
-                return implementation.getConstructor();
-            } catch (NoSuchMethodException e) {
-                throw new IllegalComponentException();
-            }
-        });
+        providers.put(type, new ConstructorInjectionSupplier<>(implementation));
     }
 
     private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
@@ -78,12 +62,12 @@ public class ContextConfig {
         };
     }
 
-    class ConstructorInjectionSupplier<T> implements ComponentProvider<T> {
+    static class ConstructorInjectionSupplier<T> implements ComponentProvider<T> {
         private final Constructor<T> injectConstructor;
 
-        @Inject
-        public ConstructorInjectionSupplier(Constructor<T> injectConstructor) {
-            this.injectConstructor = injectConstructor;
+
+        public ConstructorInjectionSupplier(Class<T> component) {
+            this.injectConstructor = getInjectConstructor(component);
         }
 
         @Override
@@ -102,6 +86,21 @@ public class ContextConfig {
         @Override
         public List<Class<?>> getDependencies() {
             return stream(injectConstructor.getParameters()).map(Parameter::getType).collect(Collectors.toList());
+        }
+
+        private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
+            List<Constructor<?>> injectConstructors = stream(implementation.getConstructors()).filter(
+                    c -> c.isAnnotationPresent(Inject.class)).collect(Collectors.toList());
+            if (injectConstructors.size() > 1) {
+                throw new IllegalComponentException();
+            }
+            return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> {
+                try {
+                    return implementation.getConstructor();
+                } catch (NoSuchMethodException e) {
+                    throw new IllegalComponentException();
+                }
+            });
         }
     }
 }
