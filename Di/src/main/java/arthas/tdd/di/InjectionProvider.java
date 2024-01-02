d34727e8c435d42fb7584f13f0922d288a096a29
Arthas <arthasjava@foxmail.com>
2024-01-02 23:22:39 +0800

    refactor(InjectionProvider): 抽取 defaultConstructor 方法使得代码自注释

--- Di/src/main/java/arthas/tdd/di/InjectionProvider.java
+++ Di/src/main/java/arthas/tdd/di/InjectionProvider.java
@@ -67,13 +67,17 @@ class InjectionProvider<T> implements ComponentProvider<T> {
         if (injectConstructors.size() > 1) {
             throw new IllegalComponentException();
         }
-        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> {
-            try {
-                return implementation.getDeclaredConstructor();
-            } catch (NoSuchMethodException e) {
-                throw new IllegalComponentException();
-            }
-        });
+        return (Constructor<Type>) injectConstructors.stream()
+                .findFirst()
+                .orElseGet(() -> defaultConstructor(implementation));
+    }
+
+    private static <Type> Constructor<Type> defaultConstructor(Class<Type> implementation) {
+        try {
+            return implementation.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new IllegalComponentException();
+        }
     }
 
     private static <T> List<Field> getInjectFields(Class<T> component) {
