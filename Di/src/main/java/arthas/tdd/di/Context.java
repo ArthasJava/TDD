09d42c39e7d198c14642ab568e70a6004b50adcb
Arthas <arthasjava@foxmail.com>
2024-01-06 12:26:37 +0800

    refactor(Ref): Ref 单独类难以理解，搬移到 Context 中作为内部类

--- Di/src/main/java/arthas/tdd/di/Context.java
+++ Di/src/main/java/arthas/tdd/di/Context.java
@@ -1,5 +1,6 @@
 package arthas.tdd.di;
 
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Optional;
 
@@ -9,4 +10,37 @@ public interface Context {
     };
 
     Optional get(Ref ref);
+
+    class Ref {
+        private Type containerType;
+        private Class<?> componentType;
+
+        public Ref(Class<?> componentType) {
+            this.componentType = componentType;
+        }
+
+        public Ref(ParameterizedType containerType) {
+            this.containerType = containerType.getRawType();
+            this.componentType = (Class<?>) containerType.getActualTypeArguments()[0];
+        }
+
+        static Ref of(Type type) {
+            if (type instanceof ParameterizedType container) {
+                return new Ref(container);
+            }
+            return new Ref((Class<?>) type);
+        }
+
+        public boolean isContainer() {
+            return containerType != null;
+        }
+
+        public Type getContainerType() {
+            return containerType;
+        }
+
+        public Class<?> getComponentType() {
+            return componentType;
+        }
+    }
 }
