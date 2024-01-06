6956543b2ad897a97fcbf2359c8966532df944eb
Arthas <arthasjava@foxmail.com>
2024-01-06 12:34:19 +0800

    refactor(ComponentProvider): 新的创建，将目标签名 List<Context.Ref> getDependencyRefs 创建

--- Di/src/main/java/arthas/tdd/di/ComponentProvider.java
+++ Di/src/main/java/arthas/tdd/di/ComponentProvider.java
@@ -2,11 +2,16 @@ package arthas.tdd.di;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.List.of;
 
 interface ComponentProvider<T> {
     T get(Context context);
+
+    default List<Context.Ref> getDependencyRefs() {
+        return getDependencies().stream().map(Context.Ref::of).collect(Collectors.toList());
+    }
 
     default List<Type> getDependencies() {
         return of();
