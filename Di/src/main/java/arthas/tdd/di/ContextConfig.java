316b996406342473fbd48c92e5e50ac327a8ad34
Arthas <arthasjava@foxmail.com>
2024-01-07 10:43:47 +0800

    refactor(ContextConfig): Map<Class<?>, ComponentProvider<?>> 替换欸 Map<Component, ComponentProvider<?>>

--- Di/src/main/java/arthas/tdd/di/ContextConfig.java
+++ Di/src/main/java/arthas/tdd/di/ContextConfig.java
@@ -10,11 +10,10 @@ import java.util.Optional;
 import java.util.Stack;
 
 public class ContextConfig {
-    private final Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
     private final Map<Component, ComponentProvider<?>> components = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        providers.put(type, (ComponentProvider<Type>) context -> instance);
+        components.put(new Component(type, null), (ComponentProvider<Type>) context -> instance);
     }
 
     public <Type> void bind(Class<Type> type, Type instance, Annotation... qualifiers) {
@@ -26,7 +25,7 @@ public class ContextConfig {
     record Component(Class<?> type, Annotation qualifier) { }
 
     public <Type, Implementation extends Type> void bind(Class<Type> type, Class<Implementation> implementation) {
-        providers.put(type, new InjectionProvider<>(implementation));
+        components.put(new Component(type, null), new InjectionProvider<>(implementation));
     }
 
     public <Type, Implementation extends Type> void bind(Class<Type> type, Class<Implementation> implementation,
@@ -38,39 +37,39 @@ public class ContextConfig {
 
     public Context getContext() {
         // 后续做校验的为止
-        providers.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
+        components.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Context() {
             @Override
             public <ComponentType> Optional<ComponentType> get(Ref<ComponentType> ref) {
-                if (ref.getQualifier() != null) {
-                    return Optional.ofNullable(
-                                    components.get(new Component(ref.getComponentType(), ref.getQualifier())))
-                            .map(componentProvider -> (ComponentType) componentProvider.get(this));
-                }
                 if (ref.isContainer()) {
                     if (ref.getContainerType() != Provider.class) {
                         return Optional.empty();
                     }
-                    return (Optional<ComponentType>) Optional.ofNullable(providers.get(ref.getComponentType()))
+                    return (Optional<ComponentType>) Optional.ofNullable(getProvider(ref))
                             .map(componentProvider -> (Provider<Object>) () -> componentProvider.get(this));
                 }
-                return Optional.ofNullable(providers.get(ref.getComponentType()))
+                return Optional.ofNullable(getProvider(ref))
                         .map(componentProvider -> (ComponentType) componentProvider.get(this));
             }
         };
     }
 
-    private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
-        for (Context.Ref dependency : providers.get(component).getDependencies()) {
-            if (!providers.containsKey(dependency.getComponentType())) {
-                throw new DependencyNotFoundException(component, dependency.getComponentType());
+    private <ComponentType> ComponentProvider<?> getProvider(Context.Ref<ComponentType> ref) {
+        return components.get(new Component(ref.getComponentType(), ref.getQualifier()));
+    }
+
+    private void checkDependencies(Component component, Stack<Class<?>> visiting) {
+        for (Context.Ref dependency : components.get(component).getDependencies()) {
+            Component dependencyComponent = new Component(dependency.getComponentType(), dependency.getQualifier());
+            if (!components.containsKey(dependencyComponent)) {
+                throw new DependencyNotFoundException(component.type(), dependency.getComponentType());
             }
             if (!dependency.isContainer()) {
                 if (visiting.contains(dependency.getComponentType())) {
                     throw new CyclicDependenciesException(visiting);
                 }
                 visiting.push(dependency.getComponentType());
-                checkDependencies(dependency.getComponentType(), visiting);
+                checkDependencies(dependencyComponent, visiting);
                 visiting.pop();
             }
         }
